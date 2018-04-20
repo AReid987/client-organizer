26c06c451eb633c8130d660d38f1bc8bdbb79eeb
Antonio Reid <read.musik@gmail.com>
2018-04-20 23:11:02 +0000

    get /signup route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,13 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_secret"
   end
 
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      redirect :'tweets'
+    else
+      erb :'users/create_stylist'
+    end
+  end
+
 end
