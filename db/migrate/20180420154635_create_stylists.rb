b11b14c38a10f4836f750eb14056ea73d2107dc9
Antonio Reid <read.musik@gmail.com>
2018-04-20 15:47:51 +0000

    stylist table migration

---
+++ db/migrate/20180420154635_create_stylists.rb
@@ -0,0 +1,9 @@
+class CreateStylists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :stylists do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest 
+    end
+  end
+end
